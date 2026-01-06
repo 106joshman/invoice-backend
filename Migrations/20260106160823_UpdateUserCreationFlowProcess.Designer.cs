5b685f38eafeacdd28b17b60997d41682136d75c
Joshua <ejembijoshman@gmail.com>
2026-01-06 17:16:56 +0100

    feat:update after migration and baseURL set

--- Migrations/20260106160823_UpdateUserCreationFlowProcess.Designer.cs
+++ Migrations/20260106160823_UpdateUserCreationFlowProcess.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InvoiceService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InvoiceService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260106160823_UpdateUserCreationFlowProcess")]
+    partial class UpdateUserCreationFlowProcess
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
